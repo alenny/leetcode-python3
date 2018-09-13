0109dfe33f14083cc37fe5900189b3c856540a94
alenny <dzy197810@hotmail.com>
2018-09-13 09:31:52 -0400

    Commit solution for maximum size subarray sum equals k

---
+++ src/maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+
+class Solution:
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sumsMap = defaultdict(list)
+        sumsMap[0] = [0]
+        sum = 0
+        for i in range(1, len(nums) + 1):
+            sum += nums[i - 1]
+            sumsMap[sum].append(i)
+        longest = 0
+        for lowSum, lowCounts in sumsMap.items():
+            highSum = lowSum + k
+            if not highSum in sumsMap:
+                continue
+            highIndexes = sumsMap[highSum]
+            longest = max(longest, highIndexes[-1] - lowCounts[0])
+        return longest
