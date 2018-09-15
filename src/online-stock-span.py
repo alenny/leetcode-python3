9887ed10764d990b48d0ed4066ef1d2571037aac
Zhuyun DAI <dzy197810@hotmail.com>
2018-09-14 22:16:43 -0400

    Commit solution for online stock span

---
+++ src/online-stock-span.py
@@ -0,0 +1,26 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.prices = []
+        self.spans = []
+
+    def next(self, price):
+        """
+        :type price: int
+        :rtype: int
+        """
+        span = 1
+        i = len(self.prices) - 1
+        while i >= 0:
+            if self.prices[i] > price:
+                break
+            span += self.spans[i]
+            i -= self.spans[i]
+        self.prices.append(price)
+        self.spans.append(span)
+        return span
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
