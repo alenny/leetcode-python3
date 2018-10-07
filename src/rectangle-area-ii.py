62549e23866ff3c6b2e8a33cf029bca396e5d52c
Zhuyun DAI <dzy197810@hotmail.com>
2018-10-07 09:55:27 -0400

    Commit solution for rectangle area ii

--- src/rectangle-area-ii.py
+++ src/rectangle-area-ii.py
@@ -1,4 +1,4 @@
-MOD_FACTOR = 10**9 + 7
+import math
 
 
 class Solution:
@@ -7,9 +7,28 @@ class Solution:
         :type rectangles: List[List[int]]
         :rtype: int
         """
-        return 0
+        modulo = 10**9 + 7
+        rectangles.sort(key=lambda rec: rec[0], reverse=True)
+        allX = list(set([rec[0] for rec in rectangles] +
+                        [rec[2] for rec in rectangles]))
+        allX.sort()
+        totalArea = 0
+        for xi in range(len(allX) - 1):
+            xLeft, xRight = allX[xi], allX[xi + 1]
+            recToHandle = []
+            while len(rectangles) > 0 and rectangles[-1][0] == xLeft:
+                recToHandle.append(rectangles.pop())
+            recToHandle.sort(key=lambda rec: rec[1])  # sort by y0
+            maxY1 = -math.inf
+            for x0, y0, x1, y1 in recToHandle:
+                totalArea += max(0, (y1 - max(maxY1, y0)) * (xRight - xLeft))
+                maxY1 = max(maxY1, y1)
+                if x1 > xRight:
+                    rectangles.append([xRight, y0, x1, y1])
+        return totalArea % modulo
 
 
 sol = Solution()
 ret = sol.rectangleArea([[0, 0, 2, 2], [1, 0, 2, 3], [1, 0, 3, 1]])
+# ret = sol.rectangleArea([[0, 0, 3, 3], [2, 0, 5, 3], [1, 1, 4, 4]])
 print(ret)
